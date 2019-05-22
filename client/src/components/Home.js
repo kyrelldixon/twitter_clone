659afa4aa8fd4685aaab0993a4d782390c9fdcb1
Godfrey <godfreyjr@gmail.com>
2019-05-21 23:07:47 -0500

    Added footer to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -31,6 +31,29 @@ const Home = () => (
         </div>
       </section>
 
+      <footer>
+        <ul>
+          <li>About</li>
+          <li>Help Center</li>
+          <li>Blog</li>
+          <li>Status</li>
+          <li>Jobs</li>
+          <li>Terms</li>
+          <li>Privacy Policy</li>
+          <li>Cookies</li>
+          <li>Ads Info</li>
+          <li>Brand</li>
+          <li>Apps</li>
+          <li>Advertise</li>
+          <li>Marketing</li>
+          <li>Businesses</li>
+          <li>Developers</li>
+          <li>Directory</li>
+          <li>Settings</li>
+          <span>© 2019 Twitter Clone</span>
+        </ul>
+      </footer>
+
     </div>
   );
 
