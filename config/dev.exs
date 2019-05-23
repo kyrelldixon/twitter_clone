c449d181b31911f58a224d03c8cf1ab7956ae4e3
Kyrell Dixon <kyrell.brown@gmail.com>
2019-05-23 01:01:53 -0500

    Set debug errors to false and got bcrypt for user authentication

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,7 @@ use Mix.Config
 # with webpack to recompile .js and .css sources.
 config :twitter_api, TwitterApiWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: []
