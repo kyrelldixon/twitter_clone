ad47ed93b58a8420757a79d8d99c6a84a5725181
Godfrey <godfreyjr@gmail.com>
2019-05-29 21:48:17 -0500

    Created signup form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,23 @@
 import  React from "react";
 import Nav from "./Nav";
+import './SignupForm.css';
+import { Link } from "react-router-dom";
 
 const SignupForm = () => (
   <div>
     <Nav />
-    This is the signup form
+     <div className="container" id="signup-box">
+      <h1>Create your account</h1>
+      <p>Name</p>
+      <input type="text" />
+      <p>Username</p>
+      <input type="text" />
+      <p>Email</p>
+      <input type="text" />
+      <p>Password</p>
+      <input type="text" />
+      <Link to="/login"><button>Sign Up</button></Link>
+     </div>
   </div>
 );
 
