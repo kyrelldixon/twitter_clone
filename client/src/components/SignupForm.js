67513424de85265cee8c7529626352e3a9675807
Godfrey <godfreyjr@gmail.com>
2019-06-02 16:06:35 -0500

    Added axios and attempted GET request to server

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
-import  React, { useState } from "react";
+import  React, { useState, useEffect } from "react";
 import Nav from "./Nav";
 import './SignupForm.css';
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const SignupForm = () =>
 
@@ -11,32 +12,60 @@ const SignupForm = () =>
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleSubmit = (e) => {
+    const userInfo = {
+      user: {
+        name: name,
+        username: username,
+        credential: {
+          email: email,
+          password: password
+        }
+      }
+    }
+    console.log(userInfo);
+    getTest();
+    e.preventDefault();
+  }
+
+  const getTest = async () => {
+    try {
+      const response = await axios.get('localhost:4000/api/users');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+    // localhost:4000/api/users
+
   return (
     <div>
       <Nav />
-       <div className="container" id="signup-box">
-        <h1>Create your account</h1>
-        <label>
-        <p>Name</p>
-        <input type="text" value={name} onChange={e => setName(e.target.value)} />
-        </label>
+      <div className="container" id="signup-box">
+        <form action="#" id="signup" onSubmit={handleSubmit}>
+          <h1>Create your account</h1>
+          <label>
+            <p>Name</p>
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
+          </label>
 
-        <label>
-        <p>Username</p>
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-        </label>
+          <label>
+            <p>Username</p>
+            <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+          </label>
 
-        <label>
-        <p>Email</p>
-        <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
-        </label>
+          <label>
+            <p>Email</p>
+            <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+          </label>
 
-        <label>
-        <p>Password</p>
-        <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
-        </label>
+          <label>
+            <p>Password</p>
+            <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
+          </label>
 
-        <Link to="/login"><button>Sign Up</button></Link>
+          <button type="submit">Sign Up</button>
+        </form>
        </div>
     </div>
   );
