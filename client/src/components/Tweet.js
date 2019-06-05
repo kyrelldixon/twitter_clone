b34d8bb53188531f1b750060a0ecb391edffee11
Godfrey <godfreyjr@gmail.com>
2019-06-05 17:51:03 -0500

    Added styling to tweet component

--- client/src/components/Tweet.js
+++ client/src/components/Tweet.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import './Tweet.css';
 
 const Tweet = (props) => {
   return (
-      <div>
-          <div id="tweet-head">
-            <img alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
-            <div id="name">{props.name}</div>
-            <div id="username">{props.username}</div>
+      <div id="tweet-wrapper">
+          <img alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
+          <div id="tweet-container">
+            <div id="name">{props.name || "no name found"}</div>
+            <div id="username">@{props.username || "no username found"}</div>
+            <div id="time-diff"> · {props.timeDiff || "--m"}</div>
             <button><i class="fas fa-chevron-down"></i></button>
-          </div>
-          <div>
-            <div id="tweet-message">{props.text_content}</div>
+            <div id="tweet-message">{props.text_content || "no message found"}</div>
           </div>
       </div>
   );
