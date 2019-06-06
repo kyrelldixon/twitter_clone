248d844f6f71d499fea4732952d833585b0e191c
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-05 19:32:09 -0500

    Fixed migration bug

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule TwitterApi.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create", "ecto.migrate", "test"]
     ]
   end
 end
