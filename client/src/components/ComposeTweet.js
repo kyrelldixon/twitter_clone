54b970c421366beaa4594ec493164999bb7d485b
Godfrey <godfreyjr@gmail.com>
2019-06-08 16:44:41 -0500

    Created ComposeTweet component

---
+++ client/src/components/ComposeTweet.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ComposeTweet.css';
+
+const ComposeTweet = () => {
+  return (
+    <div id="compose-tweet-modal">
+      <div id="compose-tweet-head">
+        <button id="close-compose">x</button>
+        <button id="submit-tweet">Tweet</button>
+      </div>
+      <div id="compose-tweet-body">
+        <img alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
+        <textarea id="compose-tweet-message" placeholder="What's happening?"/>
+      </div>
+    </div>
+  );
+}
+
+export default ComposeTweet;
