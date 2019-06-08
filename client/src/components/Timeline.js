6a26bdfcf7883ab50aa8676590898adf2ba83901
Godfrey <godfreyjr@gmail.com>
2019-06-08 14:51:30 -0500

    Updated styling for compose box

--- client/src/components/Timeline.js
+++ client/src/components/Timeline.js
@@ -25,10 +25,13 @@ const Timeline = () => {
       <div id="home-bar">Home<i className="far fa-star"></i></div>
       <div id="compose">
         <img alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
-        <input type="text" placeholder="What's happening?" />
+        <div id="input-box">What's happening?</div>
+        <i className="far fa-image"></i>
+        <i className="far fa-file-video"></i>
+        <i className="far fa-chart-bar"></i>
       </div>
       <ul id="tweets-ul">
-        {tweetData.map(tweet => <li key={tweet.tweet_id}><Tweet userID={tweet.user_id} text={tweet.text}/></li>)}
+        {tweetData.reverse().map(tweet => <li key={tweet.tweet_id}><Tweet userID={tweet.user_id} text={tweet.text}/></li>)}
       </ul>
     </div>
   );
