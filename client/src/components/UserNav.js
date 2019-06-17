bbdaaf576ec4c85a3e61ed682e17f90302a9eb4a
Godfrey <godfreyjr@gmail.com>
2019-06-17 00:55:46 -0500

    Updated styling for user nav objects

--- client/src/components/UserNav.js
+++ client/src/components/UserNav.js
@@ -4,19 +4,19 @@ import { Link } from "react-router-dom";
 
 const UserNav = () => (
   <nav id="user-nav">
-    <div className="userhome-container">
+    <div className="userhome-nav-container">
       <ul>
-        <li> <i className="fas fa-home"></i> </li>
+        <li> <Link to="#" className="nav-link"><i className="fas fa-home"></i></Link> </li>
         <li> <Link to="#" className="nav-link"><i className="fas fa-hashtag"></i></Link> </li>
         <li> <Link to="#" className="nav-link"><i className="far fa-bell"></i></Link> </li>
         <li> <Link to="#" className="nav-link"><i className="far fa-envelope"></i></Link> </li>
       </ul>
       <input id="search-box" placeholder="Search Twitter" />
-      <div id="acct-link">
+      <span id="acct-link">
         <img alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
         <span>Name</span>
         <i className="fas fa-chevron-down"></i>
-      </div>
+      </span>
     </div>
   </nav>
 )
