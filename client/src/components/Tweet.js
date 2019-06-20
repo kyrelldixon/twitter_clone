1b1db35f45160591b3c76c2c244ed53b9daf6744
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-19 21:30:07 -0500

    Removed extra indent

--- client/src/components/Tweet.js
+++ client/src/components/Tweet.js
@@ -2,22 +2,22 @@ import React from 'react';
 import './Tweet.css';
 
 const Tweet = (props) => (
-      <div id="tweet-wrapper">
-          <img alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
-          <div id="tweet-container">
-            <div id="name">{props.name}</div>
-            <div id="username">@{props.username || "no username found"}</div>
-            <div id="time-diff"> · {props.timeDiff || "--m"}</div>
-            <button><i className="fas fa-chevron-down"></i></button>
-            <div id="tweet-message">{props.text || "no message found"}</div>
-            <div id="tweet-foot">
-              <i className="far fa-comment"> {props.commentCount || ""}</i>
-              <i className="fas fa-retweet"> {props.retweetCount || ""}</i>
-              <i className="far fa-heart"> {props.likeCount || ""}</i>
-              <i className="far fa-share-square"></i>
-            </div>
-          </div>
+  <div id="tweet-wrapper">
+    <img alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
+    <div id="tweet-container">
+      <div id="name">{props.name}</div>
+      <div id="username">@{props.username || "no username found"}</div>
+      <div id="time-diff"> · {props.timeDiff || "--m"}</div>
+      <button><i className="fas fa-chevron-down"></i></button>
+      <div id="tweet-message">{props.text || "no message found"}</div>
+      <div id="tweet-foot">
+        <i className="far fa-comment"> {props.commentCount || ""}</i>
+        <i className="fas fa-retweet"> {props.retweetCount || ""}</i>
+        <i className="far fa-heart"> {props.likeCount || ""}</i>
+        <i className="far fa-share-square"></i>
       </div>
-  );
+    </div>
+  </div>
+);
 
 export default Tweet;
