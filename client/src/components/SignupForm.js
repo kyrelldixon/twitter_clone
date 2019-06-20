4439c640ece3adeb12f6010dab3378903c6b4e2e
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-20 14:50:06 -0500

    Refactored sign up form to use useForm hook

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,74 +1,69 @@
-import  React, { useState } from "react";
-import Nav from "./Nav";
-import './SignupForm.css';
+import  React from "react";
+import axios from "axios";
 import { withRouter } from "react-router-dom";
-import axios from 'axios';
+import Nav from "./Nav";
+import { useForm } from "../hooks/useForm";
+
+import './SignupForm.css';
 
 const SignupForm = ({ history }) =>
-
 {
-  const [name, setName] = useState('');
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (e) => {
+  const { values, handleChange, handleSubmit } = useForm(signup);
+  
+  async function createUser() {
     const user = {
       user: {
-        name: name,
-        username: username,
+        name: values.name,
+        username: values.username,
         credential: {
-          email: email,
-          password: password
+          email: values.email,
+          password: values.password
         }
       }
     }
-    createUser(user);
-    history.push('/login');
-    e.preventDefault();
-  }
-
-  const createUser = async (user) => {
+    
     try {
       const response = await axios.post('http://localhost:4000/v1/users', user);
       console.log(response);
+      return true;
     } catch (error) {
       console.error(error);
+      return false;
     }
+  }
+
+  function signup() {
+    const isSuccess = createUser();
+    if (isSuccess) history.push('/login');
   }
 
   return (
     <div>
       <Nav />
       <div className="container" id="signup-box">
-        <form action="#" id="signup" onSubmit={handleSubmit}>
+        <form id="signup" onSubmit={handleSubmit}>
           <h1>Create your account</h1>
           <label>
             <p>Name</p>
-            <input type="text" value={name} onChange={e => setName(e.target.value)} />
+            <input type="text" name="name" value={values.name} onChange={handleChange} required />
           </label>
-
           <label>
             <p>Username</p>
-            <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            <input type="text" name="username" value={values.username} onChange={handleChange} required />
           </label>
-
           <label>
             <p>Email</p>
-            <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="text" name="email" value={values.email} onChange={handleChange} required />
           </label>
-
           <label>
             <p>Password</p>
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            <input type="password" name="password" value={values.password} onChange={handleChange} required />
           </label>
-
           <button type="submit">Sign Up</button>
         </form>
        </div>
     </div>
   );
-
 }
 
 export default withRouter(SignupForm);
