dd769fe7fc2c57c98c77490c58c6d4f782a7c6ec
Godfrey <godfreyjr@gmail.com>
2019-06-19 22:05:42 -0500

    Added display state, useRef and useEffect for tweet menu functionality

--- client/src/components/Tweet.js
+++ client/src/components/Tweet.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Tweet.css';
+import TweetMenu from './TweetMenu';
 
-const Tweet = (props) => (
+const Tweet = (props) => {
+  const [menuDisplayState, setMenuDisplayState] = useState(false);
+
+  return (
       <div id="tweet-wrapper">
           <img alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
           <div id="tweet-container">
             <div id="name">{props.name}</div>
             <div id="username">@{props.username || "no username found"}</div>
             <div id="time-diff"> · {props.timeDiff || "--m"}</div>
-            <button id="tweet-menu-btn"><i className="fas fa-chevron-down"></i></button>
+            <button id="tweet-menu-btn" onClick={() => setMenuDisplayState(true)}>
+              <i className="fas fa-chevron-down"></i>
+            </button>
+            <TweetMenu display={menuDisplayState} handleDisplay={setMenuDisplayState} />
             <div id="tweet-message">{props.text || "no message found"}</div>
             <div id="tweet-foot">
               <i className="far fa-comment"> {props.commentCount || ""}</i>
@@ -19,5 +26,6 @@ const Tweet = (props) => (
           </div>
       </div>
   );
+}
 
 export default Tweet;
--- client/src/components/TweetMenu.js
+++ client/src/components/TweetMenu.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './TweetMenu.css';
 
 const TweetMenu = (props) => {
+  
+  const node = useRef();
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClose);
+    return () => {
+      document.removeEventListener('mousedown', handleClose);
+    }
+   }, []);
+
+   const handleClose = e => {
+    console.log(node.current);
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    props.handleDisplay(false);
+  };
 
   return (
-      <div id="tweet-menu-box">
-        <button class="tweet-menu-btn">View Tweet Activity</button>
-        <button class="tweet-menu-btn">Embed Tweet</button>
-        <button class="tweet-menu-btn">Pin to your profile</button>
-        <button class="tweet-menu-btn" id="delete-tweet">Delete</button>
+      <div ref={node} id="tweet-menu-box" className={props.display ? "showMenu" : "hideMenu"}>
+        <button className="tweet-menu-btn">View Tweet Activity</button>
+        <button className="tweet-menu-btn">Embed Tweet</button>
+        <button className="tweet-menu-btn">Pin to your profile</button>
+        <button className="tweet-menu-btn" id="delete-tweet">Delete</button>
       </div>
   );
 }
