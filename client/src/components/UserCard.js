5b9db12acf6d52f81c17261d07cf428b1dd33fc4
Godfrey <godfreyjr@gmail.com>
2019-06-21 15:51:06 -0500

    Created action button component and added it to user card component

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './UserCard.css';
+import ActionButton from './ActionButton';
 
 const UserCard = (props) => {
 
@@ -8,9 +9,11 @@ const UserCard = (props) => {
       <img alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
       <div>
         <div id="name">{props.name || "no name found"}</div>
+        <div id="btn-box">
+          <ActionButton text="Follow" />
+        </div>
         <div id="username">@{props.username || "no username found"}</div>
       </div>
-      <button>Follow</button>
     </div>
   );
 }
