1b79b5b270e3afdf99dbaa9a8e43288bc1d08508
Godfrey <godfreyjr@gmail.com>
2019-06-21 00:24:44 -0500

    Added UserCard component

---
+++ client/src/components/UserCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './UserCard.css';
+
+const UserCard = () => {
+
+  return (
+    <div id="usercard-wrapper">
+      <div>user info</div>
+      <button>follow</button>
+    </div>
+  );
+}
+
+export default UserCard;
--- client/src/components/WhoToFollow.js
+++ client/src/components/WhoToFollow.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './WhoToFollow.css';
+import UserCard from './UserCard';
 
 const WhoToFollow = () => {
 
   return (
     <div>
       <div id="follow-who-header">Who to follow</div>
+      <UserCard />
     </div>
   );
 }
