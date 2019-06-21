15a4bf0b3c565e8ff9d72714d082b27c2eb7d048
Godfrey <godfreyjr@gmail.com>
2019-06-21 12:55:03 -0500

    Added functionality to request all users and updated UserCard component

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './UserCard.css';
 
-const UserCard = () => {
+const UserCard = (props) => {
 
   return (
     <div id="usercard-wrapper">
-      <div>user info</div>
-      <button>follow</button>
+      <img alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
+      <div>
+        <div id="name">{props.name || "no name found"}</div>
+        <div id="username">@{props.username || "no username found"}</div>
+      </div>
+      <button>Follow</button>
     </div>
   );
 }
--- client/src/components/WhoToFollow.js
+++ client/src/components/WhoToFollow.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './WhoToFollow.css';
 import UserCard from './UserCard';
+import axios from 'axios';
 
 const WhoToFollow = () => {
+
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const userListObj = await axios.get('http://localhost:4000/v1/users');
+        setUserData(userListObj.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    
+    getUsers();
+  }, []);
 
   return (
     <div>
       <div id="follow-who-header">Who to follow</div>
-      <UserCard />
+      <ul id="tweets-ul">
+        {userData.map(user => <li key={user.id}>
+          <UserCard name={user.name} username={user.username} id={user.id} />
+          </li>)}
+      </ul>
     </div>
   );
 }
