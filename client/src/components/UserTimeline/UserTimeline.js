30d2213ee06894f0e466d2d064d4f3a104c097ac
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-22 18:34:06 -0500

    Getting data dor user and user timeline

--- client/src/components/UserTimeline/UserTimeline.js
+++ client/src/components/UserTimeline/UserTimeline.js
@@ -7,19 +7,30 @@ import './UserTimeline.css';
 const UserTimeline = (props) => {
 
   const [userTweets, setUserTweets] = useState([]);
+  const [user, setUser] = useState({})
 
   useEffect(() => {
     const getUserTweets = async () => {
       try {
-        const tweetObj = await axios.get('http://localhost:4000/v1/tweets');
-        setUserTweets(tweetObj.data.data);
+        const response = await axios.get(`http://localhost:4000/v1/tweets/user_timeline?username=${props.match.params.username}`);
+        setUserTweets(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    const getUser = async () => {
+      try {
+        const response = await axios.get(`http://localhost:4000/v1/users/show?username=${props.match.params.username}`);
+        setUser(response.data.data);
       } catch (error) {
         console.log(error);
       }
     }
     
     getUserTweets();
-  }, []);
+    getUser();
+  }, [props.match.params.username]);
 
   return (
     <div id="user-acct">
@@ -28,12 +39,12 @@ const UserTimeline = (props) => {
       </div>
       <div id="acct-main">
         <img id="user-photo" alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
-        <div id="name">{props.name || "no name found"}</div>
-        <div id="username">@{props.username || "no username found"}</div>
+        <div id="name">{user.name || "no name found"}</div>
+        <div id="username">@{user.username || "no username found"}</div>
         <div id="btn-box"><ActionButton text="Edit Profile" /></div>
-        <div id="user-bio">{props.bio || "no bio found"}</div>
-        <div id="following-count">{props.following_count || "0"} Following </div>
-        <div id="follower-count">{props.follower_count || "0"} Followers</div>
+        <div id="user-bio">{user.bio || "no bio found"}</div>
+        <div id="following-count">{user.following_count || "0"} Following </div>
+        <div id="follower-count">{user.follower_count || "0"} Followers</div>
         <div id="user-tabs">
           <button className="acct-tab">Tweets</button>
           <button className="acct-tab">Tweets & replies</button>
