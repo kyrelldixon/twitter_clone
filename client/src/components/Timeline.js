41fd3f3898c97d5550dac980ed633d6ced9f9995
Godfrey <godfreyjr@gmail.com>
2019-06-22 12:25:46 -0500

    Updated ComposeTweet to use token auth and reload timeline data when tweet is created

--- client/src/components/Timeline.js
+++ client/src/components/Timeline.js
@@ -27,7 +27,7 @@ const Timeline = () => {
 
   return (
     <div>
-      <ComposeTweet display={displayState} handleDisplay={setDisplayState}/>
+      <ComposeTweet display={displayState} handleDisplay={setDisplayState} setTweetData={setTweetData}/>
       <div id="home-bar">Home<i className="far fa-star"></i></div>
       <div id="compose" onClick={() => setDisplayState(true) }>
         <img alt="user" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
