251204382f3e23544e4b52887f5f20eaeb62df0a
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-23 11:17:15 -0500

    Added semi-colons

--- client/src/services/apiClient.js
+++ client/src/services/apiClient.js
@@ -4,10 +4,10 @@ export const LOCAL_STORAGE_KEY = '__twitter_api_token__';
 const API_URL = process.env.API_URL || 'http://localhost:4000';
 
 const client = (endpoint, {data, ...customConfig} = {}, useToken = true) => {
-  const headers = {'content-type': 'application/json'}
+  const headers = {'content-type': 'application/json'};
   
   const token = window.localStorage.getItem(LOCAL_STORAGE_KEY);
-  if (token && useToken) headers.Authorization = `Bearer ${token}`
+  if (token && useToken) headers.Authorization = `Bearer ${token}`;
   
   const config = {
     method: data ? 'post' : 'get',
@@ -17,7 +17,7 @@ const client = (endpoint, {data, ...customConfig} = {}, useToken = true) => {
       ...headers,
       ...customConfig.headers,
     }
-  }
+  };
 
   if (data) config.data = data;
 
