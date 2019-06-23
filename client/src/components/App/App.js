dcd078211225f07acf1bf075d6e0fce50d2dfeca
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-23 16:14:05 -0500

    Fixed import names

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
-import './App.css';
-import LandingPage from '../../routes/LandingPage/LandingPage';
-import TimelinePage from '../../routes/TimelinePage/TimelinePage';
+import LandingPage from '../../routes/LandingPage';
+import TimelinePage from '../../routes/TimelinePage';
 import LoginPage from '../../routes/LoginPage';
 import SignupPage from '../../routes/SignupPage';
-import Footer from '../Footer/Footer';
+import Footer from '../Footer';
+
+import './App.css';
 
 const App = () => (
   <Switch>
