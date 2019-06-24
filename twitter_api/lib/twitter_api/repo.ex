d6e3a6878ffeb79f22727689f5fd1fdb19b970ec
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-24 03:58:24 -0500

    Removed backend

--- twitter_api/lib/twitter_api/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule TwitterApi.Repo do
-  use Ecto.Repo,
-    otp_app: :twitter_api,
-    adapter: Ecto.Adapters.Postgres
-end
--- twitter_api/priv/repo/migrations/20190606001128_rename_text_content_column.exs
+++
@@ -1,7 +0,0 @@
-defmodule TwitterApi.Repo.Migrations.RenameTextContentColumn do
-  use Ecto.Migration
-
-  def change do
-    rename table(:tweets), :text_content, to: :text
-  end
-end
