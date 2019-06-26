38db587cbdbc00285521ee64adcbc9a7c8e0f349
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-26 06:29:16 -0500

    Made nav responsive

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,9 +7,9 @@ const Nav = () => (
   <nav>
     <div className="container">
       <ul>
-        <li> <NavLink to="/" className="nav-link"><i className="fab fa-twitter" id="nav-logo"></i><span>Home</span></NavLink> </li>
-        <li> <NavLink to="/" className="nav-link"> <span>About</span> </NavLink> </li>
-        <li id="nav-right" className="nav-link"> <NavLink to="#"><span>Language: English</span></NavLink> </li>
+        <li className="home"> <NavLink to="/" className="nav-link"><i className="fab fa-twitter" id="nav-logo"></i><span>Home</span></NavLink> </li>
+        <li className="about"> <NavLink to="/" className="nav-link"> <span>About</span> </NavLink> </li>
+        <li className="nav-link pull-right"> <NavLink to="/login"><span>Language: English</span></NavLink> </li>
       </ul>
     </div>
   </nav>
