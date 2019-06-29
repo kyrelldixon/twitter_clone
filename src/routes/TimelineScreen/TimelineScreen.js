804eaf5865e71355e0ddc767190f4bae23bf5792
Godfrey <godfreyjr@gmail.com>
2019-06-28 23:52:06 -0500

    Updated user image to link to usermenu and adjusted styling

--- src/routes/TimelineScreen/TimelineScreen.js
+++ src/routes/TimelineScreen/TimelineScreen.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch,Route } from 'react-router-dom'
 import UserNav from '../../components/UserNav';
 import Timeline from '../../components/Timeline';
 import WhoToFollow from '../../components/WhoToFollow';
 import UserTimeline from '../../components/UserTimeline';
+import UserMenu from '../../components/UserMenu';
 import { useAuth } from '../../hooks';
 import { generateRandomIconUrl } from '../../services/randomImageClient';
 
 const TimelineScreen = () => {
 
   const [{ isAuthenticated }, ] = useAuth();
+  const [menuDisplayState, setMenuDisplayState] = useState();
 
   return (
     <div>
       <div id="home-bar-nav" className="home-bar">
-        {isAuthenticated && <img className="user-profile-img" alt="user" src={generateRandomIconUrl()} />}Home
+        {isAuthenticated && <img className="user-profile-img"
+          onClick={() => setMenuDisplayState(true)} alt="user" src={generateRandomIconUrl()} />}Home
         <i className="far fa-star"></i>
       </div>
+      <UserMenu display={menuDisplayState} handleDisplay={setMenuDisplayState} />
       <UserNav />
       <div className="home-container">
         <div className="left-side">
